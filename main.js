183173e3aace759fda0e92f6c8925ea3c00cbc54
n1niraj <n1vaya@torontomu.ca>
2024-10-03 12:11:27 -0400

    added the comments

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// enlarge image 
 function toggleImage() {
   const image = document.getElementById('profile-pic');
   if (image.style.transform == 'scale(3)') {
@@ -6,11 +7,12 @@ function toggleImage() {
     image.style.transform = 'scale(3)';
   }
 }
-
+// Input name 
 function enterName() {
   var name = prompt("Enter your name");
   document.getElementById('name').innerHTML = 'Hello, ' + name + ' welcome to my page!';
 }
+//onmouseover/onmouseout 
 function changeColor(num) {
   if (num == 1) {
     document.getElementById("navColor1").style.color = "red";
@@ -19,7 +21,7 @@ function changeColor(num) {
   } else if (num == 3) {
     document.getElementById("navColor3").style.color = "red";
   } 
-}
+}//onmouseover/onmouseout 
 function revertColor(num) {
   if (num == 1) {
     document.getElementById("navColor1").style.color = "rgb(5, 205, 255)";
